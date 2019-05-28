5c6800d3d3b8d75716b07cb7fb74ec6b444a124c
Alexey Shmelkov <ashmelkov@aligntech.com>
2019-05-28 18:55:28 +0300

    [lesson-25] class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class App extends React.Component {
+    render(){
+      return (
+        <div>
+            <Header username= 'Alex'/>
+            <Greeting />
+        </div>
+      );
+    };
+};
+
+class Header extends React.Component {
+    render() {
+        return (
+        <header>
+            <p>Welcome, {this.props.username}!</p>
+        </header>
+        );
+    };
+};
+
+
+
+class Greeting extends Component {
+    render() {
+        let timeOfDay
+        const date = new Date()
+        const hours = date.getHours()
+        if (hours < 12) {
+            timeOfDay = "morning";
+        } else if (hours >= 12 && hours < 17) {
+            timeOfDay = "afternoon";
+        } else {
+            timeOfDay = "night";
+        }
+        return(
+            <h1>Good {timeOfDay} to you, sir or madam!</h1>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
